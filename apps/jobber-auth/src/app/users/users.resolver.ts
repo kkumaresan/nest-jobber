cd8bf521202813a2ff528544c8f675edf7d85639
Kumaresan <ux.kumaresan@gmail.com>
2025-10-11 11:47:33 +0530

    Implement user creation and retrieval functionality with validation

--- apps/jobber-auth/src/app/users/users.resolver.ts
+++ apps/jobber-auth/src/app/users/users.resolver.ts
@@ -1,11 +1,21 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { User } from './models/user.model';
 import { UsersService } from './users.service';
+import { CreateUserInput } from './dto/create-user.input';
 
 @Resolver(() => User)
 export class UsersResolver {
   constructor(private readonly usersService: UsersService) {}
 
+  @Mutation(() => User)
+  async createUser(
+    @Args('createUserInput') createUserInput: CreateUserInput
+  ) {
+    return this.usersService.createUser(createUserInput);
+  }
+
   @Query(() => [User], { name: 'users' })
-  async getUsers(){}
+  async getUsers() {
+    return this.usersService.getUsers();
+  }
 }
