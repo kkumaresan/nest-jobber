ea722467592a7c783a754b0266652dda574fa328
Kumaresan <ux.kumaresan@gmail.com>
2025-10-11 17:07:20 +0530

    JWT setup

--- apps/jobber-auth/src/app/users/users.service.ts
+++ apps/jobber-auth/src/app/users/users.service.ts
@@ -8,15 +8,19 @@ export class UsersService {
   constructor(private readonly prismaService: PrismaService) {}
 
   async createUser(data: Prisma.UserCreateInput) {
-    return this.prismaService.user.create({ 
+    return this.prismaService.user.create({
       data: {
         ...data,
-        password: await hash(data.password, 10)
-      }
+        password: await hash(data.password, 10),
+      },
     });
   }
 
   async getUsers() {
     return this.prismaService.user.findMany();
+  }
+
+  async getUser(args: Prisma.UserWhereUniqueInput) {
+    return this.prismaService.user.findUniqueOrThrow({ where: args });
   }
 }
--- readme/instructions.md
+++ readme/instructions.md
@@ -1,2 +1,23 @@
 npx create-nx-workspace --preset nest --name jobber --appName jobber-auth
 sudo npm i -g nx
+
+---
+
+mutation {
+createUser(createUserInput: {
+email: "kumar@kk.com"
+password: "Strongpass1@3"
+}) {
+id
+email
+}
+}
+
+mutation {
+login(loginInput: {
+email: "kumar@kk.com"
+password: "Strongpass1@3"
+}) {
+id
+}
+}
