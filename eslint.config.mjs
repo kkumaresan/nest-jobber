4003d80929a007985bda91054e3be915577a5b90
Kumaresan <ux.kumaresan@gmail.com>
2025-10-14 09:46:48 +0530

    eslint error fix for allowed paths

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,23 +9,24 @@ export default [
   },
   {
     files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
-    // rules: {
-    //   '@nx/enforce-module-boundaries': [
-    //     'error',
-    //     {
-    //       enforceBuildableLibDependency: true,
-    //       allow: ['^.*/eslint(\\.base)?\\.config\\.[cm]?[jt]s$', '**/*'],
-    //       depConstraints: [
-    //         {
-    //           sourceTag: '*',
-    //           onlyDependOnLibsWithTags: ['*'],
-    //         },
-    //       ],
-    //     },
-    //   ],
-    // },
     rules: {
-      '@nx/enforce-module-boundaries': 'off',
+      '@nx/enforce-module-boundaries': [
+        'error',
+        {
+          enforceBuildableLibDependency: true,
+          allow: [
+            '^.*/eslint(\\.base)?\\.config\\.[cm]?[jt]s$',
+            '^types(/.*)?$',
+            '@jobber/nestjs',
+          ],
+          depConstraints: [
+            {
+              sourceTag: '*',
+              onlyDependOnLibsWithTags: ['*'],
+            },
+          ],
+        },
+      ],
     },
   },
   {
